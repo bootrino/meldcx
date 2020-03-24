6d066317f7ccba42550a141de701a46e28593f94
bootrino <bootrino@gmail.com>
2020-03-24 12:33:03 +1100

    Delete App.js

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Login from "./components/Login"
-import DevicesContainer from "./components/DevicesContainer";
-
-function App() {
-  return (localStorage.getItem('authtoken') === null) ? <Login/> : <DevicesContainer/>;
-}
-
-export default App;
